758031cb4ff32af7c669d27696ca8bea777c702b
lugyin <lugyin@gmail.com>
2015-03-01 12:11:18 -0800

    halfway

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,64 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+  var my_stringify = function(xx) {
+
+	if(typeof xx === "number") {
+		return '"' + xx + '"';
+	} 
+
+
+	if(typeof xx === "boolean") {
+		return '"' + xx + '"';
+	} 
+
+	if(typeof xx === "string") {
+		return '"' + xx + '"';
+	} 
+
+	if(Array.isArray(xx)) {
+		
+		if(xx.length === 0) {
+			return ' " [] " ';
+		} 
+
+		if( xx.length === 1) { 
+		return '[' + my_stringify(xx[0]) + ']';
+		} 
+
+		if(xx.length > 1) { 
+			var aa = '[' + my_stringify(xx[0]);
+			for(var i = 1; i<xx.length; i++) {
+			aa = aa + ", " + my_stringify(xx[i]);
+			}
+			return aa + ']';
+		}	
+		
+	} 
+
+	if(typeof xx === "object") {
+	var keys = Object.keys(xx);
+	console.log(keys)	
+		// for {}
+
+		if(keys.length === 0) {
+			return ' {} ';
+		}	
+		
+		if(keys.length === 1) {
+			for(var p in xx) {
+					return '{"' + p + '" : ' + my_stringify(xx[p]) + '}';
+				}
+		}
+
+		if(keys.length > 1) {
+			var aa = "{" + [keys[0]] + ':' + my_stringify(xx[keys[0]])
+			for(var p in xx) {
+					aa '"' + p + '" : ' + my_stringify(xx[p]) + '}';
+				}
+		}	
+	} 
+
 };
+
+{"a":["b", "c"]},
